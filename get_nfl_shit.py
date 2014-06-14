9a615678c481a3c355c1f79d6beeb1a857e0ae8d
Sahil Patel <spatel7@berkeley.edu>
2014-06-13 22:20:55 -0700

    Changed it again

--- get_nfl_shit.py
+++ get_nfl_shit.py
@@ -1,7 +1,7 @@
 import pymongo
 from pymongo import MongoClient
 
-client = MongoClient('mongodb://localhost:27017/') # this is for local. For production, I used MongoClient("spoton.it", 27272)
+client = MongoClient('mongodb://spoton.it:27272/') # this is for local. For production, I used MongoClient("spoton.it", 27272)
 db = client["calendar"]
 nfl_teams = db.buttoncategories.find({"shortname": {"$regex": "nfl"}})
 activity_pools = [team['activities'] for team in nfl_teams]
